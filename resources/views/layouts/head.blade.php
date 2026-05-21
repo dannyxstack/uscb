7650493b77b5006cb3772c502148426da061d4e7
dannyxstack <danielxu1100@gmail.com>
2026-05-21 21:13:33 +0800

    update agents

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <title>{{ $title ? $title.' - ': '' }} Unity Skills College Of Hospitality</title>
+    <title>{{ empty($title) ? '' : $title.' - ' }} Unity Skills College Of Hospitality</title>
 
     @include('layouts.meta')
 
